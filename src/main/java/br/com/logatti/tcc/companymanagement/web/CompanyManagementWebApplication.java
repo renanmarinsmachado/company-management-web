4e6ccc03afd0f2bcd68cf41f64b022ebba9053ca
renan machado <renan machado>
2016-07-11 11:11:35 -0300

    add feature Employee

--- src/main/java/br/com/logatti/tcc/companymanagement/web/CompanyManagementWebApplication.java
+++ src/main/java/br/com/logatti/tcc/companymanagement/web/CompanyManagementWebApplication.java
@@ -2,6 +2,9 @@ package br.com.logatti.tcc.companymanagement.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @SpringBootApplication
 public class CompanyManagementWebApplication{
@@ -10,4 +13,12 @@ public class CompanyManagementWebApplication{
 		SpringApplication.run(CompanyManagementWebApplication.class, args);
 	}
 	
+	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+	    FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+	    CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+	    characterEncodingFilter.setEncoding("UTF-8");
+	    registrationBean.setFilter(characterEncodingFilter);
+	    return registrationBean;
+	}
 }
--- src/main/java/br/com/logatti/tcc/companymanagement/web/enums/Role.java
+++ src/main/java/br/com/logatti/tcc/companymanagement/web/enums/Role.java
@@ -1,5 +1,5 @@
 package br.com.logatti.tcc.companymanagement.web.enums;
 
 public enum Role {
-	USER, ADMIN
+	USER, EMPLOYEE
 }
